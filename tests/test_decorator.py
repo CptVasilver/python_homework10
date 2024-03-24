6311e61763c683d782a9620d0cb4a91cc1bb7039
Mikhail Vasiliev <m89154280096@yandex.ru>
2024-03-24 17:36:17 +0300

    Homework10 done

--- tests/test_decorator.py
+++ tests/test_decorator.py
@@ -1,7 +1,5 @@
 import allure
-from selene.support import by
-from selene.support.conditions import be
-from selene.support.shared import browser
+from selene import browser, be, by
 from selene.support.shared.jquery_style import s
 
 
