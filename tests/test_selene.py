217888dfeeebb3cf08a23d0c5f46aa6c5e7d282f
Mikhail Vasiliev <m89154280096@yandex.ru>
2024-03-24 16:38:22 +0300

    troubles

---
+++ tests/test_selene.py
@@ -0,0 +1,16 @@
+from selene.support import by
+from selene.support.conditions import be
+from selene.support.shared import browser
+from selene.support.shared. jquery_style import s
+
+
+def test_github():
+    browser.open("https://github.com/")
+
+    s(by.text('Search or jump to...')).click()
+    s('#query-builder-test').send_keys("CptVasilver/python_homework8")
+    s('#query-builder-test').submit()
+
+    s(by.link_text("CptVasilver/python_homework8")).click()
+    s("#issues-tab").click()
+    s(by.partial_text("#1")).should(be.visible)
